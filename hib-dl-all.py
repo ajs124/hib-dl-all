423e703260557b220c4e96c7540ed0efdc2cdb2a
ajs124 <git@ajs124.de>
2016-07-04 19:06:31 +0200

    begin to add torrent watchdir support

--- hib-dl-all.py
+++ hib-dl-all.py
@@ -4,12 +4,19 @@ import argparse
 import os
 import subprocess
 
+def dl(filename, url):
+    if not os.path.isfile(filename):
+        subprocess.run(["wget", "-c", "-O", filename, url]).check_returncode()
+
 parser = argparse.ArgumentParser(description='downloads your whole humble bundle library.')
 parser.add_argument('-u', metavar='user@mail.com', help='your humblebundle.com username', dest='username')
 parser.add_argument('-p', metavar='hunter2', help='your humblebundle.com password', dest='password')
 # not yet implemented
 #parser.add_argument('-k', metavar='KEY', help='humble key for a purchase or bundle', dest='keys', nargs='*')
-parser.add_argument('-d', metavar='DIRECTORY', help='your games will be saved here', dest='directory', default='.')
+parser.add_argument('-d', metavar='DIRECTORY', help='your games, soundtracks, ebooks, … will be saved here', dest='directory', default='.')
+parser.add_argument('-w', metavar='watchdir', help='torrent files will be put here', dest='watchdir', default=None)
+#parser.add_argument('-t', help='only download torrent files (falls back to normal download if no torrent available)', dest='torrent_only', action='store_true')
+#parser.set_defaults(torrent_only=False)
 
 args = parser.parse_args()
 #if(args.keys == None and args.username == None):
@@ -29,8 +36,12 @@ for order in order_list:
             if subproduct.downloads != None:
                 for download in subproduct.downloads:
                     for download_struct in download.download_struct:
-                        url = download_struct.url.web
-                        if url != None:
-                           filename = url.split("/")[3].split("?")[0]
-                           if not os.path.isfile(filename):
-                               subprocess.run(["wget", "-c", "-O", filename, url]).check_returncode()
+                        torrent_url = download_struct.url.bittorrent
+                        web_url = download_struct.url.web
+                        if args.watchdir != None and torrent_url != None:
+                           filename = args.watchdir + "/" + torrent_url.split("/")[4].split("?")[0]
+                           dl(filename, torrent_url)
+                        if torrent_url != None and web_url != None: # and not torrent_only
+                           filename = web_url.split("/")[3].split("?")[0]
+                           dl(filename, web_url)
+
